7f2e4f97ea1928691cf69199f5b42576decaca0e
Steph Symosko <steph.symosko@gmail.com>
2017-07-30 13:40:05 -0400

    add team page

---
+++ wp-content/themes/dw-bootstrap-child/template-team.php
@@ -0,0 +1,170 @@
+<?php
+/*
+* Template Name: Team Template
+*/
+?>
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="agora-team">
+  <div class="container-fluid">
+    <div class="row row-eq-height">
+      <div class="col-xs-12 col-sm-6 col-md-6 hp-grid-height red-bg">
+        <div>
+          <p class="text-uppercase header-title">Our Team</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-6 no-padding">
+        <img class="img-responsive" src="http://via.placeholder.com/640x348"/>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Content Layout -->
+
+<div id="agora-team" class="container-fluid">
+	<div class="row row-eq-height">
+    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 tan-bg">
+			<div class="team">
+        <p>The cornerstone of Agora's success is our staff of experienced industry professionals who, combined, span every discipline in a project's life cycle-from site selection, feasibility studies and acquisition, to contracting, construction, property and asset management. These in-house capabilities provide a seamless blend with Agora's highly practical business model to consistently maximize client/investor value as well as project efficiency, affording us the opportunity to maximize value at each stage of a project's life cycle.</p>
+        <div class="row team-member-info">
+         <div class="col-xs-12 col-sm-3 col-md-4 col-sm-offset-1 col-md-offset-0">
+           <div class="team-member">
+             <div class="team-member-img"><img src="http://via.placeholder.com/160x175"/></div>
+             <div class="team-member-name">Cary J. Lefton</div>
+             <div class="team-member-position">Chief Executive Officer</div>
+             <div class="team-member-bio-link text-uppercase">
+               <a href="#">Bio &rarr;</a>
+             </div>
+             <div class="team-member-bio">
+               <div class="bio-img"></div>
+               <div class="bio-name"></div>
+               <div class="bio-position"></div>
+               <div class="bio-info"></div>
+             </div>
+           </div>
+         </div>
+         <div class="col-xs-12 col-sm-3 col-md-4 col-sm-offset-1 col-md-offset-0">
+          <div class="team-member">
+             <div class="team-member-img"><img src="http://via.placeholder.com/160x175"/></div>
+             <div class="team-member-name">Andy A. Cole</div>
+             <div class="team-member-position">Controller</div>
+             <div class="team-member-bio-link text-uppercase">
+               <a href="#">Bio &rarr;</a>
+             </div>
+             <div class="team-member-bio"></div>
+           </div>
+         </div>
+         <div class="col-xs-12 col-sm-3 col-md-4 col-sm-offset-1 col-md-offset-0">
+           <div class="team-member">
+             <div class="team-member-img"><img src="http://via.placeholder.com/160x175"/></div>
+             <div class="team-member-name">Ryan Lefton</div>
+             <div class="team-member-position">Chief Operations Officer</div>
+             <div class="team-member-bio-link text-uppercase">
+               <a href="#">Bio &rarr;</a>
+             </div>
+             <div class="team-member-bio"></div>
+           </div>
+         </div>
+        </div>
+
+        <div class="row team-member-info">
+          <div class="col-xs-12 col-sm-3 col-md-4 col-sm-offset-1 col-md-offset-0">
+            <div class="team-member">
+              <div class="team-member-img"><img src="http://via.placeholder.com/160x175"/></div>
+              <div class="team-member-name">Cesar De La Cruz</div>
+              <div class="team-member-position">Property Management &amp; Leasing</div>
+              <div class="team-member-bio-link text-uppercase">
+                <a href="#">Bio &rarr;</a>
+              </div>
+              <div class="team-member-bio"></div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-4 col-sm-offset-1 col-md-offset-0">
+            <div class="team-member">
+              <div class="team-member-img"><img src="http://via.placeholder.com/160x175"/></div>
+              <div class="team-member-name">Ted Horwith</div>
+              <div class="team-member-position">Senior Vice President</div>
+              <div class="team-member-bio-link text-uppercase">
+                <a href="#">Bio &rarr;</a>
+              </div>
+              <div class="team-member-bio"></div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-4 col-sm-offset-1 col-md-offset-0">
+            <div class="team-member">
+              <div class="team-member-img"><img src="http://via.placeholder.com/160x175"/></div>
+              <div class="team-member-name">Aaron Lefton</div>
+              <div class="team-member-position">Acquisitions &amp; Leasing</div>
+              <div class="team-member-bio-link text-uppercase">
+                <a href="#">Bio &rarr;</a>
+              </div>
+              <div class="team-member-bio"></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row team-member-info">
+          <div class="col-xs-12 col-sm-3 col-md-4 col-sm-offset-1 col-md-offset-0">
+            <div class="team-member">
+              <div class="team-member-img"><img src="http://via.placeholder.com/160x175"/></div>
+              <div class="team-member-name">Eileen Lopez</div>
+              <div class="team-member-position">Administration Assistant</div>
+              <div class="team-member-bio-link text-uppercase">
+                <a href="#">Bio &rarr;</a>
+              </div>
+              <div class="team-member-bio"></div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-4 col-sm-offset-1 col-md-offset-0">
+            <div class="team-member">
+              <div class="team-member-img"><img src="http://via.placeholder.com/160x175"/></div>
+              <div class="team-member-name">Michiyo Matsuo</div>
+              <div class="team-member-position">Vice President of Investments</div>
+              <div class="team-member-bio-link text-uppercase">
+                <a href="#">Bio &rarr;</a>
+              </div>
+              <div class="team-member-bio"></div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-3 col-md-4 col-sm-offset-1 col-md-offset-0">
+            <div class="team-member">
+              <div class="team-member-img"><img src="http://via.placeholder.com/160x175"/></div>
+              <div class="team-member-name">Michiyo Matsuo</div>
+              <div class="team-member-position">Vice President of Investments</div>
+              <div class="team-member-bio-link text-uppercase">
+                <a href="#">Bio &rarr;</a>
+              </div>
+              <div class="team-member-bio"></div>
+            </div>
+          </div>
+        </div>
+
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 darkbrown-bg">
+      <div class="blank-block"></div>
+    </div>
+	</div>
+  <div class="row">
+    <div class="col-xs-12 no-padding">
+      <img class="img-responsive" src="http://via.placeholder.com/1280x348"/>
+    </div>
+  </div>
+</div>
+
+
+<?php endwhile; else : ?>
+    <div class="container-fluid">
+        <div class="row row-eq-height">
+            <div class="col-lg-12">
+              <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
